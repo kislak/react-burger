0781bfca2c048efc408ef8d69ce369146782297e
Serge Kislak <kislak7@gmail.com>
2022-05-17 15:49:58 +0300

    add scroll

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -6,26 +6,29 @@ import './burger-ingredients.css';
 import data from "../../utils/data"
 
 function BurgerIngredients() {
-    const [current, setCurrent] = React.useState('one')
+    const [current, setCurrent] = React.useState('bun')
+
+    React.useEffect(() => {
+        document.querySelector(`#${current}Ref`).scrollIntoView({
+            behavior: 'smooth'
+        });
+    },[current])
 
     return (
         <section className="burger-ingredients">
             <h1 className="mt-10 text text_type_main-large">Соберите бургер</h1>
             <nav className="burger-ingredients__nav">
-                <Tab value="one" active={current === 'one'} onClick={setCurrent}>
-                    Булки
-                </Tab>
-                <Tab value="two" active={current === 'two'} onClick={setCurrent}>
-                    Соусы
-                </Tab>
-                <Tab value="three" active={current === 'three'} onClick={setCurrent}>
-                    Начинки
-                </Tab>
+                <Tab value="bun" active={current === 'bun'} onClick={setCurrent}>Булки</Tab>
+                <Tab value="sauce" active={current === 'sauce'} onClick={setCurrent}>Соусы</Tab>
+                <Tab value="main" active={current === 'main'} onClick={setCurrent}>Начинки</Tab>
             </nav>
 
             <div className="burger-ingredient__sections custom-scroll">
-                <IngredientSection title="Булки" items={data.filter(i => i.type === "bun")}/>
+                <a id="bunRef"></a>
+                <IngredientSection title="Булки" items={data.filter(i => i.type === "bun")} />
+                <a id="sauceRef"></a>
                 <IngredientSection title="Соусы" items={data.filter(i => i.type === "sauce")} />
+                <a id="mainRef"></a>
                 <IngredientSection title="Начинки" items={data.filter(i => i.type === "main")} />
             </div>
         </section>
