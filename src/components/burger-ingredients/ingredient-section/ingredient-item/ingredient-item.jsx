3b2adef8aad1b3db05059cf065047744a84cc75f
Serge Kislak <kislak7@gmail.com>
2022-05-17 13:13:00 +0300

    ingr markup

--- src/components/burger-ingredients/ingredient-section/ingredient-item/ingredient-item.jsx
+++ src/components/burger-ingredients/ingredient-section/ingredient-item/ingredient-item.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './ingredient-item.css';
+import {CurrencyIcon, Counter} from "@ya.praktikum/react-developer-burger-ui-components";
 
 function IngredientItem({item}) {
     return (
         <section className="ingredient-item">
-            {item.name}
+            <Counter count={1} size="default" />
+            <img src={item.image} />
+            <div className="ingredient-item__price-section">
+                <span className="text text_type_digits-default m-1">
+                    {item.price}
+                </span>
+                <CurrencyIcon />
+            </div>
+            <div className="ingredient-item__name text text_type_main-default">
+                {item.name}
+            </div>
         </section>
     )
 }
--- src/components/burger-ingredients/ingredient-section/ingredient-section.jsx
+++ src/components/burger-ingredients/ingredient-section/ingredient-section.jsx
@@ -4,12 +4,13 @@ import IngredientItem from "./ingredient-item/ingredient-item";
 
 function IngredientSection({title, items}) {
     return (
-        <section className="ingredient-section">
-            {title}
-            {items.map(item => {
-                return <IngredientItem key={item.id} item={item} />
-            }) }
-            <hr/>
+        <section className="ingredient-section mt-10">
+            <h2 className="text text_type_main-medium">{title}</h2>
+            <div className="ingredient-section__items">
+                {items.map(item => {
+                    return <IngredientItem key={item._id} item={item} />
+                }) }
+            </div>
         </section>
     )
 }
