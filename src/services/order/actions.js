282c8326769be1f7d26d6f558781364261e48952
Serge Kislak <kislak7@gmail.com>
2022-06-10 23:14:08 +0300

    add loader

--- src/services/order/actions.js
+++ src/services/order/actions.js
@@ -3,10 +3,18 @@ import { addErrorAction } from "../_middleware/errorHandler";
 import { resetOrderConstructorAction } from "../burger-constructor/actions";
 export const SUBMIT_ORDER = "SUBMIT_ORDER_SUCCESS";
 export const OPEN_ORDER_DETAILS = "OPEN_ORDER_DETAILS";
+export const SHOW_LOADER = "SHOW_LOADER";
 
 const submitOrderAction = (payload) => {
   return {
     type: SUBMIT_ORDER,
+    payload: payload,
+  };
+};
+
+const showLoaderAction = (payload) => {
+  return {
+    type: SHOW_LOADER,
     payload: payload,
   };
 };
@@ -24,14 +32,17 @@ export const submitOrder = (topItem, midItems) => (dispatch) => {
       return i._id;
     })
   );
+  dispatch(showLoaderAction(true));
   api
     .submitOrder(ingredients)
     .then((response) => {
+      dispatch(showLoaderAction(false));
       dispatch(submitOrderAction(response.order.number));
       dispatch(openOrderDetailsAction(true));
       dispatch(resetOrderConstructorAction());
     })
     .catch((error) => {
+      dispatch(showLoaderAction(false));
       dispatch(addErrorAction(error));
     });
 };
--- src/services/order/reducer.js
+++ src/services/order/reducer.js
@@ -1,8 +1,9 @@
-import { SUBMIT_ORDER, OPEN_ORDER_DETAILS } from "./actions";
+import { SUBMIT_ORDER, OPEN_ORDER_DETAILS, SHOW_LOADER } from "./actions";
 
 const initialState = {
   orderNumber: null,
   orderDetailsOpen: false,
+  showLoader: false,
 };
 
 export const reducer = (state = initialState, action) => {
@@ -16,6 +17,11 @@ export const reducer = (state = initialState, action) => {
       return {
         ...state,
         orderDetailsOpen: action.payload,
+      };
+    case SHOW_LOADER:
+      return {
+        ...state,
+        showLoader: action.payload,
       };
     default:
       return state;
