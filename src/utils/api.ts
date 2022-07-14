bcc51552b1154e7d90b604b4547ca77c2cd909bd
Serge Kislak <kislak7@gmail.com>
2022-07-14 15:16:58 +0300

    api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,30 +1,30 @@
+import {TBurgerItem} from "../prop-types/burger-item";
+
 const BASE_URL = "https://norma.nomoreparties.space/api";
 
 class Api {
-  constructor({ baseUrl }) {
-    this._baseUrl = baseUrl;
-  }
+  constructor(private baseUrl: string) {}
 
   getIngredients() {
-    return fetch(`${this._baseUrl}/ingredients`, {
+    return fetch(`${this.baseUrl}/ingredients`, {
       headers: {
         "Content-Type": "application/json",
       },
-    }).then((res) => this._getResponseData(res));
+    }).then((res) => this.getResponseData(res));
   }
 
-  submitOrder(ingredients) {
-    return fetch(`${this._baseUrl}/orders`, {
+  submitOrder(ingredients: Array<TBurgerItem>) {
+    return fetch(`${this.baseUrl}/orders`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ ingredients: ingredients }),
-    }).then((res) => this._getResponseData(res));
+    }).then((res) => this.getResponseData(res));
   }
 
-  register(email, password, name) {
-    return fetch(`${this._baseUrl}/auth/register`, {
+  register(email: string, password: string, name: string) {
+    return fetch(`${this.baseUrl}/auth/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -34,11 +34,11 @@ class Api {
         password,
         name,
       }),
-    }).then((res) => this._getResponseData(res));
+    }).then((res) => this.getResponseData(res));
   }
 
-  login(email, password) {
-    return fetch(`${this._baseUrl}/auth/login`, {
+  login(email: string, password: string) {
+    return fetch(`${this.baseUrl}/auth/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -47,11 +47,11 @@ class Api {
         email,
         password,
       }),
-    }).then((res) => this._getResponseData(res));
+    }).then((res) => this.getResponseData(res));
   }
 
   refreshToken() {
-    return fetch(`${this._baseUrl}/auth/token`, {
+    return fetch(`${this.baseUrl}/auth/token`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -59,11 +59,11 @@ class Api {
       body: JSON.stringify({
         token: localStorage.getItem("refreshToken"),
       }),
-    }).then((res) => this._getResponseData(res));
+    }).then((res) => this.getResponseData(res));
   }
 
-  logout(token) {
-    return fetch(`${this._baseUrl}/auth/logout`, {
+  logout(token: string) {
+    return fetch(`${this.baseUrl}/auth/logout`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -72,42 +72,42 @@ class Api {
       body: JSON.stringify({
         token: localStorage.getItem("refreshToken"),
       }),
-    }).then((res) => this._getResponseData(res));
+    }).then((res) => this.getResponseData(res));
   }
 
-  getUser(token) {
-    return fetch(`${this._baseUrl}/auth/user`, {
+  getUser(token: string) {
+    return fetch(`${this.baseUrl}/auth/user`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
         Authorization: token,
       },
-    }).then((res) => this._getResponseData(res));
+    }).then((res) => this.getResponseData(res));
   }
 
-  updateUser(token, email, name) {
-    return fetch(`${this._baseUrl}/auth/user`, {
+  updateUser(token: string, email: string, name: string) {
+    return fetch(`${this.baseUrl}/auth/user`, {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
         Authorization: token,
       },
       body: JSON.stringify({ email, name }),
-    }).then((res) => this._getResponseData(res));
+    }).then((res) => this.getResponseData(res));
   }
 
-  passwordReset(email) {
-    return fetch(`${this._baseUrl}/password-reset`, {
+  passwordReset(email: string) {
+    return fetch(`${this.baseUrl}/password-reset`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ email }),
-    }).then((res) => this._getResponseData(res));
+    }).then((res) => this.getResponseData(res));
   }
 
-  passwordResetSubmit(password, token) {
-    return fetch(`${this._baseUrl}/password-reset/reset`, {
+  passwordResetSubmit(password: string, token: string) {
+    return fetch(`${this.baseUrl}/password-reset/reset`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -116,10 +116,10 @@ class Api {
         password,
         token,
       }),
-    }).then((res) => this._getResponseData(res));
+    }).then((res) => this.getResponseData(res));
   }
 
-  _getResponseData(res) {
+  private getResponseData(res: Response) {
     if (res.ok) {
       return res.json();
     }
@@ -127,8 +127,5 @@ class Api {
   }
 }
 
-const api = new Api({
-  baseUrl: BASE_URL,
-});
-
+const api = new Api(BASE_URL);
 export default api;
