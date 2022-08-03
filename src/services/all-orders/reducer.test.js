8eb4a84ea0da16c3aca34c8093c48b02b6a1f268
Serge Kislak <kislak7@gmail.com>
2022-08-03 19:25:42 +0300

    pt

--- src/services/all-orders/reducer.test.js
+++ src/services/all-orders/reducer.test.js
@@ -1,68 +1,60 @@
-import { reducer } from './reducer'
+import { reducer } from "./reducer";
 import { wsOpen, wsClose, wsMessage, wsError, wsConnecting } from "./actions";
 
-
-describe('all-orders reducer', () => {
+describe("all-orders reducer", () => {
   let initialState = {
-    "connectionError": "",
-    "orders": [],
-    "status": "OFFLINE",
-    "total": 0,
-    "totalToday": 0
-  }
+    connectionError: "",
+    orders: [],
+    status: "OFFLINE",
+    total: 0,
+    totalToday: 0,
+  };
 
-  it('returns the initial state', () => {
-    expect(reducer(undefined, {})).toEqual(
-      initialState)
-  })
+  it("returns the initial state", () => {
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
 
-  it('dispatches wsConnecting', () => {
-    expect(reducer(initialState, wsConnecting)).toEqual(
-      {
-        "connectionError": "",
-        "orders": [],
-        "status": "CONNECTING...",
-        "total": 0,
-        "totalToday": 0
-      })
-  })
+  it("dispatches wsConnecting", () => {
+    expect(reducer(initialState, wsConnecting)).toEqual({
+      connectionError: "",
+      orders: [],
+      status: "CONNECTING...",
+      total: 0,
+      totalToday: 0,
+    });
+  });
 
-  it('dispatches wsOpen', () => {
-    expect(reducer(initialState, wsOpen)).toEqual(
-      {
-        "connectionError": "",
-        "orders": [],
-        "status": "ONLINE",
-        "total": 0,
-        "totalToday": 0
-      })
-  })
+  it("dispatches wsOpen", () => {
+    expect(reducer(initialState, wsOpen)).toEqual({
+      connectionError: "",
+      orders: [],
+      status: "ONLINE",
+      total: 0,
+      totalToday: 0,
+    });
+  });
 
-  it('dispatches wsOpen', () => {
-    expect(reducer(initialState, wsOpen)).toEqual(
-      {
-        "connectionError": "",
-        "orders": [],
-        "status": "ONLINE",
-        "total": 0,
-        "totalToday": 0
-      })
-  })
+  it("dispatches wsOpen", () => {
+    expect(reducer(initialState, wsOpen)).toEqual({
+      connectionError: "",
+      orders: [],
+      status: "ONLINE",
+      total: 0,
+      totalToday: 0,
+    });
+  });
 
+  it("dispatches wsClose", () => {
+    expect(reducer(initialState, wsClose)).toEqual({
+      connectionError: "",
+      orders: [],
+      status: "OFFLINE",
+      total: 0,
+      totalToday: 0,
+    });
+  });
 
-  
-  it('dispatches wsClose', () => {
-    expect(reducer(initialState, wsClose)).toEqual(
-      {
-        "connectionError": "",
-        "orders": [],
-        "status": "OFFLINE",
-        "total": 0,
-        "totalToday": 0
-      })
-  })
-
-  it('dispatches wsError', () => {
+  it("dispatches wsError", () => {
     expect(reducer(initialState, wsError("oops"))).toEqual({
       connectionError: "oops",
       orders: [],
@@ -70,21 +62,21 @@ describe('all-orders reducer', () => {
       total: 0,
       totalToday: 0,
     });
-  })
+  });
 
-  it('dispatches wsMessage', () => {
+  it("dispatches wsMessage", () => {
     let playload = {
-      orders: [{test: true}],
+      orders: [{ test: true }],
       total: 100,
-      totalToday: 1
+      totalToday: 1,
     };
 
     expect(reducer(initialState, wsMessage(playload))).toEqual({
       connectionError: "",
-      orders: [{test: true}],
+      orders: [{ test: true }],
       status: "OFFLINE",
       total: 100,
       totalToday: 1,
     });
-  })
-})
+  });
+});
