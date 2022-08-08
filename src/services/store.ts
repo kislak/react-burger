b207b26c63cbe10c57937f2d3943bf6ab00a32b1
Serge Kislak <kislak7@gmail.com>
2022-08-08 20:09:48 +0300

    review store

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,31 +1,27 @@
-import { applyMiddleware, createStore, Store } from "redux";
 import rootReducer from "./reducer";
-import { composeWithDevTools } from "redux-devtools-extension";
 import { errorHandler } from "./_middleware/error-handler";
 import { webSocketHandler } from "./_middleware/web-socket-handler";
-import thunkMiddleware from "redux-thunk";
 import { allOrdersActions } from "./all-orders/actions";
 import { profileOrdersActions } from "./profile-orders/actions";
+import { configureStore } from '@reduxjs/toolkit'
 
 const ALL_ORDERS_URL = "wss://norma.nomoreparties.space/orders/all";
 const PROFILE_ORDERS_URL = "wss://norma.nomoreparties.space/orders";
-
 const initialState = {
   ingredientsStore: { ingredients: [] },
 };
 
-export const configureStore = (): Store => {
-  const store = createStore(
-    rootReducer,
-    initialState,
-    composeWithDevTools(
-      applyMiddleware(
-        errorHandler(),
-        webSocketHandler(ALL_ORDERS_URL, allOrdersActions),
-        webSocketHandler(PROFILE_ORDERS_URL, profileOrdersActions),
-        thunkMiddleware
-      )
-    )
-  );
-  return store;
-};
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+      getDefaultMiddleware()
+          .prepend(errorHandler())
+          .prepend(webSocketHandler(PROFILE_ORDERS_URL, profileOrdersActions))
+          .prepend(webSocketHandler(ALL_ORDERS_URL, allOrdersActions)),
+  devTools: true,
+  preloadedState: initialState
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
